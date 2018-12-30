952ae8e7a1dc8fb7a1b46d61679ee9576f03fdcc
Mike Daley <michaelcdaley@yahoo.com>
2018-12-30 11:54:24 -0800

    Added icons for find place and share place. Fixed bug with Android and not both Android and iOS are working

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -2,48 +2,57 @@
 // src/screens/MainTabs/startMainTabs.js
 //-----------------------------------------------------------------------------
 import { Navigation } from 'react-native-navigation'
+import Icon           from 'react-native-vector-icons/Ionicons'
 
-const startMainTabs = () => Navigation.setRoot({
-  root: {
-    component: {
-        name: 'awesome-places.FindPlaceScreen',
-    },
-    bottomTabs: {
-      children: [
-        {
-          component: {
+const startMainTabs = () => {
+  Promise.all([
+    Icon.getImageSource("md-map", 30),
+    Icon.getImageSource("ios-share-alt", 30)
+  ]).then(sources => {
+    Navigation.setRoot({
+      root: {
+        component: {
             name: 'awesome-places.FindPlaceScreen',
-            options: {
-              title: {
-                text:     'Find Place',
-                fontSize: 20,
-                color:    'blue'
-              },
-              bottomTab: {
-                text:     'Find Place'
-              }
-            }
-          }
         },
-        {
-          component: {
-            name: 'awesome-places.SharePlaceScreen',
-            options: {
-              title: {
-                text:     'Share Place',
-                fontSize: 20,
-                color:    'purple'
-              },
-              bottomTab: {
-                text:     'Share Place'
+        bottomTabs: {
+          children: [
+            {
+              component: {
+                name: 'awesome-places.FindPlaceScreen',
+                options: {
+                  title: {
+                    text:     'Find Place',
+                    fontSize: 20,
+                    color:    'blue',
+                  },
+                  bottomTab: {
+                    text:     'Find Place',
+                    icon:     sources[0],
+                  },
+                }
+              }
+            },
+            {
+              component: {
+                name: 'awesome-places.SharePlaceScreen',
+                options: {
+                  title: {
+                    text:     'Share Place',
+                    fontSize: 20,
+                    color:    'purple'
+                  },
+                  bottomTab: {
+                    text:     'Share Place',
+                    icon:     sources[1],
+                  }
+                }
               }
             }
-          }
+          ]
         }
-      ]
-    }
-  }
-})
-
+      }
+    })
+  })
+}
 
 export default startMainTabs
